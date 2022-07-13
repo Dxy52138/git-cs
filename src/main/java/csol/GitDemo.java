9e7b53adc1f7e10f896021f2cb0d8938ac34c39c
Layne <Dxy@csol.com>
2022-07-13 14:40:51 +0800

    gitee commit

--- src/main/java/csol/GitDemo.java
+++ src/main/java/csol/GitDemo.java
@@ -6,5 +6,8 @@ public class GitDemo {
         System.out.println("22222222222222222222");
         System.out.println("33333333333333333333");
         System.out.println("4444444444444444444");
+        System.out.println("5555555555555555555");
+        System.out.println("66666666666666666666");
+        System.out.println("7777777777777gitee");
     }
 }
