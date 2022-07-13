05409ac62e9c9148b15b02f916b777e86665805a
Layne <Dxy@csol.com>
2022-07-13 10:25:21 +0800

    hot-fix2

--- src/main/java/csol/GitDemo.java
+++ src/main/java/csol/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
         System.out.println("Hello Git 第一次见面!");
         System.out.println("Hello Git 第二次见面!");
         System.out.println("Hot-fix Git 第一次见面!");
+        System.out.println("Hot-fix Git 第二次见面!");
     }
 }
