bf6d4bfae7df9d5bc387b805612c9dc4444f7b22
Layne <Dxy@csol.com>
2022-07-13 11:13:15 +0800

    GitHub commit2

--- src/main/java/csol/GitDemo.java
+++ src/main/java/csol/GitDemo.java
@@ -3,8 +3,8 @@ package csol;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("GitHub仓库 修改数据");
-        System.out.println("panzer");
-        System.out.println("seaways");
-        System.out.println("bigbang");
+        System.out.println("panzer_坦克");
+        System.out.println("seaways_水上漂");
+        System.out.println("bigbang_爆炸");
     }
 }
