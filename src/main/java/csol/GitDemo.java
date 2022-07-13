30da3be04e69581c2837606cf67d674f92c27fec
Layne <Dxy@csol.com>
2022-07-13 10:26:29 +0800

    commit3

--- src/main/java/csol/GitDemo.java
+++ src/main/java/csol/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("Hello Git 第二次见面!");
         System.out.println("Hot-fix Git 第一次见面!");
         System.out.println("Hot-fix Git 第二次见面!");
+        System.out.println("Hello Git 第三次见面!");
     }
 }
