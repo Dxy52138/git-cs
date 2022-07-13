79f1dc1f074e245f3b3f4672dd56a84213217e7e
Layne <Dxy@csol.com>
2022-07-13 10:20:35 +0800

    commit2

--- src/main/java/csol/GitDemo.java
+++ src/main/java/csol/GitDemo.java
@@ -3,5 +3,6 @@ package csol;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git 第一次见面!");
+        System.out.println("Hello Git 第二次见面!");
     }
 }
