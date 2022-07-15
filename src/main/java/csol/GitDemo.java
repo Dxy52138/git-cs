7ab3b6fdb15bc6cea09e5c1b030d774a3734e38e
Layne <Dxy@csol.com>
2022-07-15 17:41:09 +0800

    test commit20

--- src/main/java/csol/GitDemo.java
+++ src/main/java/csol/GitDemo.java
@@ -2,18 +2,6 @@ package csol;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("1111111111111111111");
-        System.out.println("22222222222222222222");
-        System.out.println("33333333333333333333");
-        System.out.println("4444444444444444444");
-        System.out.println("5555555555555555555");
-        System.out.println("66666666666666666666");
-        System.out.println("7777777777777gitee");
-        System.out.println("8888888888888gitee");
-        System.out.println("9999999999999gitee");
-        System.out.println("10gitee");
-        System.out.println("霸气");
-        System.out.println("花楼");
-        System.out.println("360");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
 }
